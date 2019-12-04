26ad64dcc9db023444a05e8c0ad5b34626394d3d
Lukas Deutz <lukas.deutz@wirecard.com>
2019-12-04 09:48:08 +0100

    Update build process

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+echo
+echo "Starting build..."
+
 asciidoctor --failure-level=WARN -a linkcss -a icons=font -a toc=left -a systemtimestamp=$(date +%s) index.adoc
+
+if [[ "$?" -ne 0 ]]; then
+    echo "::error file=build.sh::Build failed."
+    exit 1
+fi
