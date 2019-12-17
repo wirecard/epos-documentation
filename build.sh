9e187a867282fc2af93f569954aa0ff59df51c94
Lukas Deutz <lukas.deutz@wirecard.com>
2019-12-17 14:51:26 +0100

    Improve workflow (add Github Pages)

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 echo
 echo "Starting build..."
 
-asciidoctor --failure-level=WARN -a linkcss -a icons=font -a toc=left -a systemtimestamp=$(date +%s) index.adoc
+asciidoctor --failure-level=WARN \
+    -a linkcss -a stylesheet=main.css -a stylesdir=css \
+    -a icons=font -a toc=left -a systemtimestamp="$(date +%s)" \
+    index.adoc
 
 if [[ "$?" -ne 0 ]]; then
     echo "::error file=build.sh::Build failed."
